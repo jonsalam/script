f4b0da4102b1020d663b236d2bda690fd25d4d52
jonsalam <jonsalam@163.com>
2020-01-04 02:08:29 +0800

    update ci-server/java

--- ci-server/java.sh
+++ ci-server/java.sh
@@ -7,8 +7,8 @@ yum install -y java-1.8.0-openjdk-devel
 HOME=$(which java |xargs ls -lrt |awk '{print $NF}' |xargs ls -lrt |awk '{print $NF}' |awk -F '/' '{for (i=1;i<=NF-4;i++) printf("%s/", $i); print $(NF-3)}')
 
 echo "JAVA_HOME=$HOME" >> /etc/profile
-echo "PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
+echo PATH='$PATH':$JAVA_HOME/bin >> /etc/profile
 assert_status
 
 source /etc/profile
-source ~/.bashrc
+source /$(whoami)/.bashrc
