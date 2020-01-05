8d01438a6309d5708a89159f65311074d5c28fdb
jonsalam <jonsalam@163.com>
2020-01-05 15:03:38 +0800

    update: ci-server/block-ip.sh

--- ci-server/block-ip.sh
+++ ci-server/block-ip.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+source ./init-helper.sh
+
+check_directory /data/middleware/block-ip.sh
+if [[ $? -eq 1 ]]; then
+	echo "--- block-ip.sh has already been installed ---"
+	exit 100
+fi
+
 mkdir -p /data/middleware
 cp block-ip.txt /data/middleware/block-ip.sh
 chmod +x /data/middleware/block-ip.sh
 
-# cron
+echo '*/1 * * * *  sh /data/middleware/block-ip.sh' >> /etc/crontab
+assert_status
+echo "--- block-ip.sh installed ---"
