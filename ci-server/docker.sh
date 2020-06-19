8188af64b433c79ff598c6a099b346d631a31c69
jonsalam <jonsalam@163.com>
2020-06-19 21:21:08 +0800

    update docker

--- ci-server/docker.sh
+++ ci-server/docker.sh
@@ -4,12 +4,14 @@ source ./init-helper.sh
 assert_command_exist docker -v
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
-assert_status
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-assert_status
 yum install -y docker-ce docker-ce-cli containerd.io
-assert_status
+cat <<EOF > /etc/docker/daemon.json
+{
+   "registry-mirrors": [
+       "https://mirror.ccs.tencentyun.com"
+  ]
+}
+EOF
 systemctl start docker
-assert_status
 systemctl enable docker
-assert_status
