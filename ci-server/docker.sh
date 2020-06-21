0c5edf0b761e70701e75eb915eb6d76c19de3333
jonsalam <jonsalam@163.com>
2020-06-22 00:35:33 +0800

    update docker

--- ci-server/docker.sh
+++ ci-server/docker.sh
@@ -6,6 +6,7 @@ assert_command_exist docker -v
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
+mkdir -p /etc/docker
 cat <<EOF > /etc/docker/daemon.json
 {
    "registry-mirrors": [
